c52bd96e356dcc07fdef6c2f0d16a426abfb311d
Christopher Smith <123132398+clsCA-86@users.noreply.github.com>
2023-06-19 19:42:25 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const App = () => {
+ const [characters, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+ useEffect(() =>{
+  axios
+    .get('https://swapi.dev/api/people/')
+    .then(res => {
+      setCharacters(res.data)
+    })
+    .catch(error =>{
+      console.log('Error fetching characters', error);
+    })
+    
+ }, []);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Wars</h1>
+      {characters.map(character =>{
+        return <div key={character.name}>{character.name}</div>
+      })}
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,75 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledChar = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: ${(pr) => pr.theme.primaryColor};
+    background-color: ${(pr) => pr.theme.thirdColor};
+    width: 43%;
+    height: 10vh;
+    border: 3px solid black;
+    text-shadow: 1px 1px 5px white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  button {
+    width: 10%;
+    height: 8vh;
+    font-size: 1.3rem;
+    color: ${(pr) => pr.theme.thirdColor};
+
+    background-color: ${(pr) => pr.theme.primaryColor};
+  }
+  ul {
+    color: black;
+    background-color: ${(pr) => pr.theme.thirdColor};
+    width: 40%;
+    border: 3px solid black;
+    align-items: flex-start;
+    font-size: 1.5rem;
+    margin-right: 0px;
+  }
+  p {
+    display: flex;
+  }
+  h3 {
+    display: flex;
+  }
+`;
+
+const Character = (props) => {
+  console.log(props);
+  const { info, openDetails } = props;
+
+  return (
+    <StyledChar>
+      <h1>{props.info.name}</h1>
+
+      <ul>
+        {" "}
+        <h3>{props.info.name} Information:</h3>
+        <p>Gender: {props.info.gender}</p>
+        <p>Height: {props.info.height}</p>
+        <p>Mass: {props.info.mass}</p>
+        <p>BirthYear: {props.info.birth_year}</p>
+        <p>Eye Color: {props.info.eye_color}</p>
+        <p>Hair Color: {props.info.hair_color}</p>
+        <p>Skin color: {props.info.skin_color}</p>
+      </ul>
+
+      {/* <button onClick={() => openDetails(info.name)}>See details</button> */}
+
+      {/* {props.info.name} */}
+    </StyledChar>
+  );
+};
+
+export default Character;
